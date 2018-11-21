7b0ce56c04253482b1b272b812c239af263f58f6
Matthew Landauer <mlandauer@gmail.com>
2018-11-21 11:53:09 +1100

    Get the basic information from one page

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,23 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
+agent = Mechanize.new
+page = agent.get("https://www.thepaperboy.com/australia/newspapers/country.cfm")
+
+page.at("table[width=700]").search("tr")[1..-1].each do |tr|
+  tds = tr.search("td")
+  name = tds[0].at("a").inner_text.strip
+  paperboy_url = (page.uri + tds[0].at("a")["href"]).to_s
+  city = tds[1].inner_text.strip
+  state = tds[2].inner_text.strip
+  language = tds[3].inner_text.strip
+  record = { name: name, paperboy_url: paperboy_url, city: city, state: state,
+             language: language }
+  p record
+end
+
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
