eb07f3a23a6be20d5a7262bb12684d5127df494a
Matthew Landauer <mlandauer@gmail.com>
2018-11-21 12:06:16 +1100

    Gets the url of the newspaper as well and saves it

--- scraper.rb
+++ scraper.rb
@@ -1,23 +1,25 @@
 require 'scraperwiki'
 require 'mechanize'
 
+def scrape_detail_page(agent, url)
+  page = agent.get(url)
+  url = page.at("table[width=900]").at("a")["href"]
+  { "url" => url }
+end
+
 agent = Mechanize.new
 page = agent.get("https://www.thepaperboy.com/australia/newspapers/country.cfm")
 
 page.at("table[width=700]").search("tr")[1..-1].each do |tr|
   tds = tr.search("td")
-  name = tds[0].at("a").inner_text.strip
-  paperboy_url = (page.uri + tds[0].at("a")["href"]).to_s
-  city = tds[1].inner_text.strip
-  state = tds[2].inner_text.strip
-  language = tds[3].inner_text.strip
-  record = { name: name, paperboy_url: paperboy_url, city: city, state: state,
-             language: language }
-  p record
+  record = {
+    "name" => tds[0].at("a").inner_text.strip,
+    "paperboy_url" => (page.uri + tds[0].at("a")["href"]).to_s,
+    "city" => tds[1].inner_text.strip,
+    "state" => tds[2].inner_text.strip,
+    "language": tds[3].inner_text.strip
+  }
+  puts record["name"]
+  record = record.merge(scrape_detail_page(agent, record["paperboy_url"]))
+  ScraperWiki.save_sqlite(["name"], record)
 end
-
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
